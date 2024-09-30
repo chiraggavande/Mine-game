be62532354220a225e5338405542e40ab89278e8
Chirag Gavande <atomicalex01@gmail.com>
2024-09-30 11:36:29 +0530

    added readme file

--- obj/Debug/net8.0/MineGame.AssemblyInfo.cs
+++ obj/Debug/net8.0/MineGame.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MineGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+683d0eda551175fd7ba8b0f28015f64ac8faa7be")]
 [assembly: System.Reflection.AssemblyProductAttribute("MineGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MineGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
